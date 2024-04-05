6795121bb70b3ea8948e1179158692cc3e8394f3
Aniket Kumar <aniketskr@gmail.com>
2024-04-06 01:32:53 +0530

    changed folder name

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const userModel = require("../models/user.model");
+const userModel = require("../utils/user.model");
 
 exports.authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
